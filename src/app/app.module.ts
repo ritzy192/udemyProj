94e842757469c0351a5a58d544eba4b6771caa90
ritzy192 <ritzy192@gmail.com>
2019-06-06 16:20:20 +0530

    code at initial state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FacebookLike } from './facebookLike';
-
 
  
 @NgModule({
@@ -20,6 +18,3 @@ import { FacebookLike } from './facebookLike';
 
 export class AppModule { 
 }
- var initialLikes = 1;
- new FacebookLike(initialLikes,true).printCount();
- new FacebookLike(initialLikes, false).printCount();

5c35c7ddbf3631598765f3b56fb86f3a84ca0c48
ritzy192 <ritzy192@gmail.com>
2019-06-10 12:57:46 +0530

    learned about directives and interpolation worked with an example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FacebookLike } from './facebookLike';
+import { CoursesComponent } from './courses.component';
 
 
  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
@@ -20,11 +21,3 @@ import { FacebookLike } from './facebookLike';
 
 export class AppModule { 
 }
- var initialLikes = 100;
- let likeIt = new FacebookLike(initialLikes,false);
- let dislikeIt = new FacebookLike(initialLikes,true);
-
- likeIt.calculateCount();
- dislikeIt.calculateCount();
- console.log(`like count : ${likeIt.count}, isselected : ${likeIt.state}`);
- console.log(`like count : ${dislikeIt.count}, isselected : ${dislikeIt.state}`);

9371b954d9217d1e85d2fefe9c378c4a94200eb2
ritzy192 <ritzy192@gmail.com>
2019-06-11 16:39:32 +0530

    Leared one way and two way binding, event, class,property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { CoursesService } from './courses.service';
+import { clearResolutionOfComponentResourcesQueue } from '@angular/core/src/metadata/resource_loading';
 
 @Component({
     selector: 'courses',
@@ -8,12 +9,18 @@ import { CoursesService } from './courses.service';
                     <li *ngFor='let course of courses'>
                     {{course}}
                     </li>
-                </ul>`
+                </ul>
+                <input [value]="email" (keyup.enter) = "email = $event.target.value; writeEmail()" />
+                `
 })
 export class CoursesComponent {
     title = 'list of courses' ;
+    email="this@that.com"
     courses;
     constructor(service: CoursesService){
         this.courses=service.getCourses();
+    }
+    writeEmail(){
+        console.log(this.email)
     }
 }

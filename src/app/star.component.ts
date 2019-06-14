d3812145394bebdbefe4c7dc47626ddccd68ab9a
Rythem Datta <Rythem.Datta@cybergindia.local>
2019-06-14 15:03:11 +0530

    removed ngoninit

--- src/app/star.component.ts
+++ src/app/star.component.ts
@@ -7,10 +7,6 @@ export class StarComponent{
     @Input('Favourite') isFavourite: boolean;
     @Output('change') change = new EventEmitter();
 
-    ngOnInit(){
-
-    }
-
     onClick(){
         this.isFavourite = !this.isFavourite;
         this.change.emit({newvalue: this.isFavourite});
